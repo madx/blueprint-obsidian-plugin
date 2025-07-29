2890b1fcd40a9774ab5623d393c19893c41f54e3
François Vaux <madx+github@yapok.org>
2025-07-29 09:12:48 +0000

    fix: Make the code a bit more compact and add error notices

--- src/commands.ts
+++ src/commands.ts
@@ -1,75 +1,70 @@
-import { App, MarkdownView } from "obsidian";
+import { App, MarkdownView, Notice } from "obsidian";
 import * as nunjucks from "nunjucks"
 
 import { SectionExtension } from "./SectionExtension";
 import { ObsidianLoader } from "./ObsidianLoader";
 import { findSectionsWithHeadings } from "./utils";
 
+class EnsureError extends Error { }
+
+function ensure<T>(value: T, message: string): NonNullable<T> {
+  if (!value) {
+    throw new EnsureError(message)
+  }
+  return value
+}
+
 async function executeCurrentFileBlueprint(app: App, checking: boolean) {
-  const view = app.workspace.getActiveViewOfType(MarkdownView);
   const obsidianLoader = new ObsidianLoader(app)
 
-  if (!view) {
-    return
-  }
-
-  const { file, data: contents } = view
-
-  if (!file) {
-    return
-  }
-
-  const metadata = app.metadataCache.getFileCache(file)
-
-  if (!metadata || !metadata.frontmatterPosition) {
-    return
-  }
-
-  const propPath = metadata.frontmatterLinks?.find(link => link.key === "blueprint")
-
-  if (!propPath) {
-    return
-  }
-
-  const linkPath = app.metadataCache.getFirstLinkpathDest(propPath?.link, file.path)
-
-  if (!linkPath) {
-    return
-  }
-
-  const blueprint = await app.vault.cachedRead(linkPath)
-  const sections = findSectionsWithHeadings(metadata, contents)
-
-  if (!blueprint) {
-    return
-  }
-
-  const frontmatter = metadata?.frontmatter || {}
-  const getSection = (sectionName: string, defaultContent = "") => sections[sectionName] || defaultContent
-  const env = new nunjucks.Environment(obsidianLoader, { autoescape: false })
-  env.addExtension("SectionExtension", new SectionExtension(getSection))
-  const template = new nunjucks.Template(blueprint, env, file.path)
-
   try {
-    const renderedContent: string = await new Promise((resolve, reject) => {
-      const renderContext = { file, frontmatter, ...frontmatter, section: getSection }
-      template.render(renderContext, (err: unknown, result: string) => {
-        if (err) {
-          return reject(err)
-        }
+    const view = ensure(app.workspace.getActiveViewOfType(MarkdownView), "Current file is not a note");
+    const file = ensure(view.file, "Current view has no attached file")
+    const metadata = ensure(app.metadataCache.getFileCache(file), `No cached metadata for ${file.basename}`)
+    const frontmatterPosition = ensure(metadata.frontmatterPosition, "File has no frontmatter")
+    const propPath = ensure(metadata.frontmatterLinks?.find(link => link.key === "blueprint"), "File has no blueprint")
+    const linkPath = ensure(app.metadataCache.getFirstLinkpathDest(propPath?.link, file.path), "Cannot find linked blueprint")
+    const blueprint = ensure(await app.vault.cachedRead(linkPath), "Can't read blueprint file")
 
-        return resolve(result)
+    const { data: contents } = view
+
+    const sections = findSectionsWithHeadings(metadata, contents)
+
+    const frontmatter = metadata?.frontmatter || {}
+    const getSection = (sectionName: string, defaultContent = "") => sections[sectionName] || defaultContent
+    const env = new nunjucks.Environment(obsidianLoader, { autoescape: false })
+    env.addExtension("SectionExtension", new SectionExtension(getSection))
+    const template = new nunjucks.Template(blueprint, env, file.path)
+
+    try {
+      const renderedContent: string = await new Promise((resolve, reject) => {
+        const renderContext = { file, frontmatter, ...frontmatter, section: getSection }
+        template.render(renderContext, (err: unknown, result: string) => {
+          if (err) {
+            return reject(err)
+          }
+
+          return resolve(result)
+        })
       })
-    })
 
-    app.vault.process(file, (contents) => {
-      const frontmatterRaw = contents.slice(0, (metadata.frontmatterPosition?.end.offset || 0) + 1)
+      app.vault.process(file, (contents) => {
+        const frontmatterRaw = contents.slice(0, (frontmatterPosition?.end.offset || 0) + 1)
 
-      return [frontmatterRaw, renderedContent].join("")
-    })
+        return [frontmatterRaw, renderedContent].join("")
+      })
+    } catch (error) {
+      console.error(error)
+    }
+
   } catch (error) {
-    console.error(error)
+    if (error instanceof EnsureError) {
+      new Notice(error.message)
+    } else {
+      console.error(error)
+    }
   }
+
 }
 
 export { executeCurrentFileBlueprint }
