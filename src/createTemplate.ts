21e408bdd177be9b08e889bc3df4bd59694852b0
François Vaux <madx+github@yapok.org>
2026-02-09 18:30:20 +0000

    feat: Add a way to resolve properties from a linked note
    Closes #14

--- src/createTemplate.ts
+++ src/createTemplate.ts
@@ -12,6 +12,22 @@ type CreateTemplate = {
   sectionsByHeading: Record<string, string>
 }
 
+function createGetFrontmatter(app: App, filePath: string) {
+  return (targetPath: string) => {
+    const cleanTargetPath = targetPath.startsWith('[[')
+      ? targetPath.slice(2, targetPath.length - 2)
+      : targetPath
+
+    const target = app.metadataCache.getFirstLinkpathDest(cleanTargetPath, filePath)
+
+    if (!target) {
+      throw `Unable to resolve ${targetPath}`
+    }
+
+    return app.metadataCache.getFileCache(target)?.frontmatter ?? {}
+  }
+}
+
 function createTemplate({ app, blueprint, filePath, sectionsByHeading }: CreateTemplate) {
   const obsidianLoader = new ObsidianLoader(app)
   const env = new nunjucks.Environment(obsidianLoader, { autoescape: false, trimBlocks: true })
@@ -20,6 +36,7 @@ function createTemplate({ app, blueprint, filePath, sectionsByHeading }: CreateT
   env.addExtension('SectionExtension', new SectionExtension(getSection))
 
   env.addGlobal('moment', moment)
+  env.addGlobal('get_frontmatter', createGetFrontmatter(app, filePath))
 
   env.addFilter('prefix_lines', prefixLines)
   env.addFilter('split', split)
